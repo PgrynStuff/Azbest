170d7e0e09423ef649580955b20cc3215d5bb409
PgrynStuff <Pgryn.Official@gmail.com>
2025-03-16 22:36:27 +0100

    Update state_machine.gd

--- scripts/state_machine/state_machine.gd
+++ scripts/state_machine/state_machine.gd
@@ -2,12 +2,10 @@ class_name StateMachine
 extends Node
 
 @export var current_state : State
-var states: Dictionary[StringName, State] = {}
 
 func _ready() -> void:
 	for child in get_children():
 		assert(child == State)
-		states[child.name] = child
 		child.transition.connect(on_child_transition)
 		
 	current_state.enter()
@@ -19,10 +17,12 @@ func _physics_process(delta: float) -> void:
 	current_state.physics_update(delta)
 
 func on_child_transition(state_name: StringName) -> void:
-	var new_state : State = states[state_name]
-	assert(new_state)
+	var state = get_node(str(state_name))
+	assert(State == state)
 	
-	if new_state != current_state:
-		current_state.exit()
-		current_state = new_state
-		current_state.enter()
+	if state == current_state:
+		return
+	
+	current_state.exit()
+	current_state = state
+	current_state.enter()
